0defb74156c519497b2ce7bc3171fbb414f73e72
Travis <Toooo123@live.ca>
2023-02-04 19:11:11 -0800

    Add i2c system funtions

--- system.h
+++ system.h
@@ -1,12 +1,20 @@
+// system.h
+// Functions that manages files and comands
 #ifndef SYSTEM_H_
 #define SYSTEM_H_
 
 #define BUFFER_MAX_LENGTH 1024
 
+// Run the given Command
 void System_runCommand(char* command);
 
+// Read/write a file, given the file path + name and buffer
 void System_readFile(char* fileName, char* buffer);
-
 void System_writeFile(char* fileName, char* buffer);
+
+// I2C functions
+int initI2cBus(char* bus, int address);
+void writeI2cReg(int i2cFileDesc, unsigned char regAddr, unsigned char value);
+unsigned char readI2cReg(int i2cFileDesc, unsigned char regAddr);
 
 #endif
